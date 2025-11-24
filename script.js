b9454fa2065189386787e94daf4f2cf7ab6abe65
alh20-crypto <alhanafnan2020@gmail.com>
2025-11-24 15:22:05 +0530

    Add calculator JS

---
+++ script.js
@@ -0,0 +1,38 @@
+const resultInput = document.getElementById('result');
+let currentInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    resultInput.value = currentInput;
+}
+
+function appendOperator(operator) {
+    if (currentInput === '' && operator !== '-') return;
+    if (currentInput.endsWith('+') || currentInput.endsWith('-') || currentInput.endsWith('*') || currentInput.endsWith('/') || currentInput.endsWith('%')) {
+        currentInput = currentInput.slice(0, -1) + operator;
+    } else {
+        currentInput += operator;
+    }
+    resultInput.value = currentInput;
+}
+
+function clearResult() {
+    currentInput = '';
+    resultInput.value = '';
+}
+
+function deleteLast() {
+    currentInput = currentInput.slice(0, -1);
+    resultInput.value = currentInput;
+}
+
+function calculateResult() {
+    try {
+        const result = eval(currentInput);
+        resultInput.value = result;
+        currentInput = result.toString();
+    } catch (error) {
+        resultInput.value = 'Error';
+        currentInput = '';
+    }
+}
